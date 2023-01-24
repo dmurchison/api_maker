4cfd2288f4728fab58bad560469022513f0efc9a
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-24 00:32:42 -0500

    config file fun

--- app/config.py
+++ app/config.py
@@ -1,9 +1,6 @@
-import os
+from pydantic import BaseSettings, PostgresDsn
 
-class Config():
-    DB_HOST = os.get_env('DB_HOST', 'localhost')
-    DB_PORT = os.get_env('DB_PORT', 5432)
-    DB_USER = os.get_env('DB_USER', 'postgres')
-    DB_PASSWORD = os.get_env('DB_PASSWORD', 'postgres')
-    DB_NAME = os.get_env('DB_NAME', 'postgres')
+class Config(BaseSettings):
+    pass
+
 
