a3bed7bc36e13f7b3dd502b67573a73861032596
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-27 15:51:40 -0500

    database and docker NOT WORKING

--- models/liked_post.py
+++ models/liked_post.py
@@ -1,23 +1,23 @@
-import datetime
-from sqlalchemy import Column, Integer, TIMESTAMP, UniqueConstraint, ForeignKeyConstraint, Index
-from models.base import Base
+# from datetime import datetime
+# from sqlalchemy import Column, Integer, TIMESTAMP, UniqueConstraint, ForeignKeyConstraint, Index
+# from models.base import Base
 
 
 
 
-class LikedPost(Base):
-    __tablename__ = "liked_post"
+# class LikedPost(Base):
+#     __tablename__ = "liked_posts"
 
-    __table_args__ = (UniqueConstraint("user_id", "post_id", name="user_post_unique"),
-                     ForeignKeyConstraint(["user_id"], ["user_id"]))
+#     __table_args__ = (UniqueConstraint("user_id", "post_id", name="user_post_unique"),
+#                      ForeignKeyConstraint(["user_id"], ["user_id"]))
 
-    id = Column(Integer, primary_key=True)
-    created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
-    user_id = Column(Integer, nullable=False)
-    post_id = Column(Integer, nullable=False)
+#     id = Column(Integer, primary_key=True)
+#     created_at = Column(TIMESTAMP, default=datetime.utcnow)
+#     user_id = Column(Integer)
+#     post_id = Column(Integer)
 
 
-Index("liked_post_user_id_idx", LikedPost.user_id)
+# Index("liked_post_user_id_idx", LikedPost.user_id)
 
 
 
