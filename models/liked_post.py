d538ad19599846def7082fa7e8d10363d32a6ad6
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-30 13:44:25 -0500

    able to upload the prebuilt models into unknown location of psql db

--- models/liked_post.py
+++ models/liked_post.py
@@ -1,23 +1,23 @@
-# from datetime import datetime
-# from sqlalchemy import Column, Integer, TIMESTAMP, UniqueConstraint, ForeignKeyConstraint, Index
-# from models.base import Base
+from datetime import datetime
+from sqlalchemy import Column, Integer, TIMESTAMP, UniqueConstraint, ForeignKeyConstraint, Index
+from models.base import Base
 
 
 
 
-# class LikedPost(Base):
-#     __tablename__ = "liked_posts"
+class LikedPost(Base):
+    __tablename__ = "liked_posts"
 
-#     __table_args__ = (UniqueConstraint("user_id", "post_id", name="user_post_unique"),
-#                      ForeignKeyConstraint(["user_id"], ["user_id"]))
+    __table_args__ = (UniqueConstraint("user_id", "post_id", name="user_post_unique"),
+                     ForeignKeyConstraint(["user_id"], ["user.id"]))
 
-#     id = Column(Integer, primary_key=True)
-#     created_at = Column(TIMESTAMP, default=datetime.utcnow)
-#     user_id = Column(Integer)
-#     post_id = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    user_id = Column(Integer)
+    post_id = Column(Integer)
 
 
-# Index("liked_post_user_id_idx", LikedPost.user_id)
+Index("liked_post_user_id_idx", LikedPost.user_id)
 
 
 
